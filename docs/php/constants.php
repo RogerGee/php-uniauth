1509666a26d0bec5fb76702940a0d44cd1f6ca29
Roger Gee <rpg11a@acu.edu>
2024-10-10 16:33:55 -0500

    Update api documentation and readme

--- docs/php/constants.php
+++ docs/php/constants.php
@@ -1,10 +1,44 @@
 <?php
 
+/**
+ * One or more $_SERVER variables are missing or incorrect.
+ */
 define('UNIAUTH_ERROR_INVALID_SERVERVARS',100);
+
+/**
+ * The uniauth session identifier could not be loaded from either the PHP
+ * session or the uniauth cookie.
+ */
 define('UNIAUTH_ERROR_NO_SESSION',101);
+
+/**
+ * The source (i.e. registrar) session of a transfer operation does not exist.
+ */
 define('UNIAUTH_ERROR_SOURCE_NOT_EXIST',102);
+
+/**
+ * The source (i.e. registrar) session of a transfer operation has not applied.
+ */
 define('UNIAUTH_ERROR_SOURCE_NOT_APPLY',103);
+
+/**
+ * The destination (i.e. applicant) session of a transfer operation does not exist.
+ */
 define('UNIAUTH_ERROR_DEST_NOT_EXIST',104);
+
+/**
+ * An unspecified error occurred during the transfer operation.
+ */
 define('UNIAUTH_ERROR_TRANSFER_FAILED',105);
+
+/**
+ * The destination session did not supply a redirect URL and the system cannot
+ * complete the transfer.
+ */
 define('UNIAUTH_ERROR_MISSING_REDIRECT',106);
+
+/**
+ * The apply operation could not determine the applicant session ID using the
+ * uniauth query parameter.
+ */
 define('UNIAUTH_ERROR_MISSING_UNIAUTH_PARAM',107);
--- docs/php/functions.php
+++ docs/php/functions.php
@@ -1,36 +1,139 @@
 <?php
 
 /**
- * Look up a uniauth session. If a $url is provided, the function redirects if no session was found.
+ * Look up the registered uniauth session.
+ *
+ * @param string $url
+ *  (optional) The URL of the registrar endpoint. If provided, the function will
+ *  redirect to this endpoint when no registered session is found; in this case,
+ *  the program will end before this function returns.
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ *
+ * @return ?array
+ *  Returns the login array if there is a registered session. Otherwise NULL is
+ *  returned if and only if $url is not set. A login array has the following
+ *  structure:
+ *    array (
+ *      'id' => 1000,
+ *      'user' => 'name',
+ *      'display' => 'User Name',
+ *    )
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_NO_SESSION
  */
 function uniauth(string $url = null,string $session_id = null) : ?array {}
 
 /**
+ * Registers an existing uniauth session using the indicated user information.
  *
+ * @param int $id
+ *  The user ID to store in the registration.
+ * @param string $name
+ *  The user name (i.e. handle) to store in the registration.
+ * @param string $display_name
+ *  The user display name to store in the registration.
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ * @param int $lifetime
+ *  (optional) Defines the session lifetime value (in seconds).
+ *
+ *  If this value is omitted or a non-positive number, then the session will
+ *  have a lifetime equal to the value of the php.ini option "uniauth.cookie"
+ *  and the uniauth cookie (if enabled) is a persistent cookie. Otherwise the
+ *  uniauth session has the specified lifetime and the cookie is
+ *  non-persistent.
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_INVALID_SERVERVARS
+ *   - UNIAUTH_ERROR_NO_SESSION
  */
 function uniauth_register(int $id,string $name,string $display_name,string $session_id = null,int $lifetime = 0) : void {}
 
 /**
+ * Links the current, registered session with its applicant session. Control
+ * is redirected back to the applicant upon success.
  *
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ *
+ * @return never
+ *  This function does not return. It bails out when redirecting upon success
+ *  and raises exceptions/errors upon failure.
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_NO_SESSION
+ *   - UNIAUTH_ERROR_SOURCE_NOT_EXIST
+ *   - UNIAUTH_ERROR_SOURCE_NOT_APPLY
+ *   - UNIAUTH_ERROR_DEST_NOT_EXIST
+ *   - UNIAUTH_ERROR_TRANSFER_FAILED
+ *   - UNIAUTH_ERROR_MISSING_REDIRECT
  */
-function uniauth_transfer(string $session_id = null) : void {}
+function uniauth_transfer(string $session_id = null) : never {}
 
 /**
+ * Determines if a registered session exists.
  *
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ *
+ * @return bool
+ *  Returns TRUE if a registered session exists; FALSE otherwise.
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_NO_SESSION
  */
 function uniauth_check(string $session_id = null) : bool {}
 
 /**
+ * Applies a foreign session ID from $_GET['uniauth'] to the current session to
+ * prepare for a future transfer operation. The current session is created if it
+ * does not exist.
  *
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_NO_SESSION
+ *   - UNIAUTH_ERROR_MISSING_UNIAUTH_PARAM
  */
 function uniauth_apply(string $session_id = null) : void {}
 
 /**
+ * Unregisters the current session.
  *
+ * @param string $session_id
+ *  (optional) The session ID to use instead of the default ID.
+ *
+ * @return bool
+ *  Returns TRUE if the session was valid and was invalidated, FALSE otherwise.
+ *
+ * @throws \Exception If superglobals cannot be activated.
+ * @throws \Uniauth\Exception
+ *  Having codes:
+ *   - UNIAUTH_ERROR_NO_SESSION
  */
 function uniauth_purge(string $session_id = null) : bool {}
 
 /**
+ * Enables use of a uniauth cookie. This overrides the default behavior of
+ * utilizing the PHP session.
  *
+ * @return string
+ *  Returns the randomly-generated ID for the cookie (i.e. the value assigned
+ *  to $_COOKIE['uniauth']).
+ *
+ * @throws \Exception If superglobals cannot be activated.
  */
 function uniauth_cookie() : string {}
